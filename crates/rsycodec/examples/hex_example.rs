482e4a0721a167e59e73a76e64fced4abcba07a4
Tsin.Ma <tsinjue@gmail.com>
2025-04-17 11:58:45 +0800

    update

--- crates/rsycodec/examples/hex_example.rs
+++ crates/rsycodec/examples/hex_example.rs
@@ -2,4 +2,8 @@ use rsycodec::hex;
 fn main() {
     let hex_string = hex::encode("Hello world!");
     println!("{}", hex_string); // Prints "48656c6c6f20776f726c6421"
+    println!(
+        "decoded: {:?}",
+        String::from_utf8(hex::decode("48656c6c6f20776f726c6421").unwrap()).unwrap()
+    );
 }
